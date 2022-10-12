3125ed168856b41995b71382c52dde9bf9c8c7c5
codek <kevaughnbatchelor@gmail.com>
2022-10-12 16:22:23 -0500

    Add Database Tables and Migrations. End Implemenet Database Section

---
+++ LeaveManagementNet6/Data/LeaveType.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagementNet6.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
