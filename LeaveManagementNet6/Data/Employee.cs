3dcf9f32ad6cf094731c259ead7f420a1cf53983
codek <kevaughnbatchelor@gmail.com>
2022-10-12 14:21:33 -0500

    extended user table

---
+++ LeaveManagementNet6/Data/Employee.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagementNet6.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; } 
+        public string? TaxId { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
