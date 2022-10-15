72aaae305db46ee62d506325f7309584c32f3c53
codek <kevaughnbatchelor@gmail.com>
2022-10-14 19:28:33 -0500

    Modified crud screens and Add Datatables and swal.

---
+++ LeaveManagementNet6/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagementNet6.Data;
+using LeaveManagementNet6.Models;
+
+namespace LeaveManagementNet6.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ LeaveManagementNet6/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementNet6.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage="Please Enter a Valid Number")]
+        public int DefaultDays { get; set; }
+    }
+}
